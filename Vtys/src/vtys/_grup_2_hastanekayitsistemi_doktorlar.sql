ecb25afee976eb280734727c20519cacbdb68973
Berfin  Yalınız <56268192+berfin22@users.noreply.github.com>
2021-12-31 01:41:26 +0300

    db

---
+++ Vtys/src/vtys/_grup_2_hastanekayitsistemi_doktorlar.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: _grup_2_hastanekayitsistemi
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `doktorlar`
+--
+
+DROP TABLE IF EXISTS `doktorlar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `doktorlar` (
+  `doktor_id` int NOT NULL AUTO_INCREMENT,
+  `doktor_adi` varchar(50) DEFAULT NULL,
+  `doktor_soyadi` varchar(50) DEFAULT NULL,
+  `klinik_id` varchar(11) DEFAULT NULL,
+  `doktor_parola` varchar(125) DEFAULT NULL,
+  PRIMARY KEY (`doktor_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `doktorlar`
+--
+
+LOCK TABLES `doktorlar` WRITE;
+/*!40000 ALTER TABLE `doktorlar` DISABLE KEYS */;
+INSERT INTO `doktorlar` VALUES (2,'Mustafa','Gündoğan','3','123456'),(5,'Deniz','Delidalga','1','41256987'),(11,'Ali','ayse','0','0'),(12,'Kevsernur','Erginsu','6','6'),(13,'Berfin','Endodonti','8','benmalim'),(14,'Hasan','Algoloji','8','asdsad'),(17,'gg','Algoloji','1','12345'),(18,'ad','soyad','5','123'),(19,'reyhan','yeter','null','121432'),(20,'Numan','Gergin','null','fdfghhmgh'),(21,'Bub','G','null','dsgbgdh'),(22,'Hatice','tıklama','null','asdasd'),(23,'Nisa','Gün','null','13213123'),(24,'Resul','Dindar','11','jejejejej');
+/*!40000 ALTER TABLE `doktorlar` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-31  1:39:47
